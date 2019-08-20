6f0d9947171cb77e19059198f1edc5a0659af655
kazu <whiterose4429@gmail.com>
2019-08-20 18:11:15 +0900

    before bootstrap4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+#念のために書いておくタイトル用のメソッド
+  # def full_title(page_title = '')
+  #   base_title = "Noxel"
+  #   if page_title.empty?
+  #     base_title
+  #   else
+  #     page_title
+  #   end
+  # end
 end
