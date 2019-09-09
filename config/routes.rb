2357021ab0ab63704928e1dc0dfd757f5e701c2e
kazu <whiterose4429@gmail.com>
2019-09-09 12:01:10 +0900

    パスワードリセット追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root 'static_pages#home'
   get 'static_pages/about'
   get    '/login',   to: 'sessions#new'
@@ -8,5 +7,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
