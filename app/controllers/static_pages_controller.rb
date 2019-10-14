86730355b6de9c26118d364984c7514af69c9d93
kazu <whiterose4429@gmail.com>
2019-10-14 16:54:11 +0900

    css修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_action :static_home, only: [:home]
   def home
+    @ranks =Novel.find(Bookmark.group(:novel_id).order('count(novel_id) desc').limit(10).pluck(:novel_id))
   end
 
   def about
